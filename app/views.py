3a6a52d01c6f841aae1d0e662da1a3a16bb5bf04
Manika Sharma <51908554+manikasharma09@users.noreply.github.com>
2019-07-08 23:36:35 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from datetime import date
 import calendar
 from calendar import HTMLCalendar
+from .models import *
+from .forms import *
 
-def index(request,year,month): #year and month are added because of the urls.py int and str path converters
+
+def index(request,year=date.today().year,month=date.today().month): #year and month are added because of the urls.py int and str path converters
 	year=int(year)
 	month=int(month)
 	if year < 1900 or year > 2099 : year=date.today().year
 	month_name=calendar.month_name[month] #month_name func from the cal module to get month name that matches month number.
-	title="My Event Ananya Calendar - %s %s" % (month_name,year)
 	cal=HTMLCalendar().formatmonth(year,month) #retrieve html formatted cal
-	return render(request,'base.html',{'title':title,'cal':cal})
+	return render(request,'calendar_base.html',{'cal':cal})
+
+def event(request):
+	submitted=False
+	if request.method=="POST":
+		form=EventForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/event/?submitted=True')
+	else:
+	    form=EventForm()
+	    if 'submitted' in request.GET:
+	        submitted=True
+	return render(request,'event.html',{'form':form,'submitted':submitted})  	
+
+def all_events(request):
+    events_list=Event.objects.all()
+    return render(request,'all_events.html',{'events_list':events_list}) 
+
+
