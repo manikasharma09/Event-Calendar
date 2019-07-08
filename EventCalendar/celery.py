a3a3c639b178a37f825b3b9988bdadc9b8f6f72f
Manika Sharma <51908554+manikasharma09@users.noreply.github.com>
2019-07-08 23:34:54 +0530

    Add files via upload

---
+++ EventCalendar/celery.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import,unicode_literals
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventCalendar.settings') 
+
+app = Celery('EventCalendar')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
