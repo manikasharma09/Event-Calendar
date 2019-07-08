0d21aebebde4297407c749a7c39641c7be366b26
Manika Sharma <51908554+manikasharma09@users.noreply.github.com>
2019-07-08 23:37:04 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path,re_path
 from . import views
+from . import contact
 
 urlpatterns = [
     #path('<int:year>/<str:month>/',views.index,name='index'), #path converters (str,int,slug,UUID,path)
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/',views.index,name='index'),
+    path('',views.index,name='index'),
+    path('contact/',contact.contact,name='contact'),
+    path('event/',views.event,name='event'),
+    path('all_events/',views.all_events,name='all_events'),
     ]
